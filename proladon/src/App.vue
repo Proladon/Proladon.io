1631025aa654a9c7d975998e20fa06962c70b99b
Proladon <proladon@gmail.com>
2020-07-07 23:29:36 +0800

    rewite | vue init

--- proladon/src/App.vue
+++ proladon/src/App.vue
@@ -1,163 +1,28 @@
 <template>
-    <div id="app">
-        <div class="index-wrapper">
-            <Home class="section" :section="current_section" />
-            <About class="section" />
-            <Skill class="section" />
-            <Awards class="section" />
-            <Youtube class="section" />
-            <Works class="section" id="sectionWorks" />
-        </div>
-
-        <div class="menu-mask" :class="{mask_hide:menu}"></div>
-        <div class="side-menu" :class="{menu_hide:menu}">
-            <p>Home</p>
-            <p>About</p>
-            <p>Skills</p>
-            <p>Awards</p>
-            <p>Works</p>
-        </div>
-        <div class="menu-btn" @click="menu = !menu">Ω</div>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-    // import _ from 'lodash'
-    import Home from '@/views/Home.vue'
-    import About from '@/views/About.vue'
-    import Skill from '@/views/Skill.vue'
-    import Awards from '@/views/Awards.vue'
-    import Youtube from '@/views/Youtube.vue'
-    import Works from '@/views/Works.vue'
-    export default {
-        components: {
-            Home,
-            About,
-            Skill,
-            Awards,
-            Youtube,
-            Works
-        },
-        data() {
-            return {
-                current_section: this.$store.state.current_section,
-                menu: false,
-            }
-        },
-        methods:{
-            toSection(sec){
-                const element = document.getElementsByClassName(sec)
-                window.scrollTo({
-                    'behavior': 'smooth',
-                    'left': 0,
-                    'top': element.offsetTop
-                })
-            }
-        },
-    }
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <style lang="scss">
-    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
-
-    * {
-        margin: 0;
-        font-family: 'Roboto', sans-serif;
-        // overflow-y: hidden;
-    }
-
-    // font
-    @font-face {
-        font-family: "AVS";
-        src: url('../src/assets/font/AV Semi-Round straight.ttf');
-    }
-
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        height: 100%;
-
-    }
-
-    body {
-        background-color: rgb(38, 38, 38);
-    }
-
-    .menu-mask {
-        position: fixed;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        width: 100%;
-        height: 100%;
-        background: black;
-        opacity: .7;
-        transition-property: all;
-        transition: ease-in-out 1s;
-    }
-
-    .mask_hide{
-        opacity: 0;
-        pointer-events: none;
-    }
-
-    .side-menu {
-        position: fixed;
-        width: 30%;
-        height: 100vh;
-        background: #2c3e50;
-        top: 0;
-        right: 0;
-        transition: ease-in-out .7s;
-
-        p {
-            color: white;
-        }
-    }
-
-    .menu_hide {
-        width: 0;
-        transition: ease-in-out .7s;
-    }
-
-    .menu-btn {
-        position: fixed;
-        color: aquamarine;
-        top: 20px;
-        right: 20px;
-        cursor: pointer;
-    }
-
-    .index_nav {
-        visibility: collapse;
-        background-color: transparent !important;
-    }
-
-    #nav {
-        padding: 30px;
-
-        a {
-            font-weight: bold;
-            color: #2c3e50;
-
-            &.router-link-exact-active {
-                color: #42b983;
-            }
-        }
-    }
-
-    .web-wrapper {
-        width: 1000px;
-    }
-
-    .page-wrapper {
-        width: calc(100% - 100px);
-        margin-left: 100px;
-        height: 100vh;
-        // overflow-y: hidden;
-    }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- proladon/src/main.js
+++ proladon/src/main.js
@@ -1,22 +1,10 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import Vueprlx from "vue-prlx";
-import VueKinesis from 'vue-kinesis';
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
 
-Vue.config.productionTip = false;
-Vue.use(Vueprlx)
-Vue.use(VueKinesis)
+Vue.config.productionTip = false
+
 new Vue({
-    created() {
-        AOS.init({
-            duration: 1000,
-        });
-    },
-    router,
-    store,
-    render: (h) => h(App),
-}).$mount("#app");
+  store,
+  render: h => h(App)
+}).$mount('#app')
